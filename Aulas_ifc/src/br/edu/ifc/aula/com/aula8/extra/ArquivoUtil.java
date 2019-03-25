327f2aa8a7d9d217b00f53de8091ca6a1f57fb7a
Gabriel <Gabriel@Gabriel-PC>
2019-03-24 21:18:58 -0300

    aula 9 fazendo exercicios

--- Aulas_ifc/src/br/edu/ifc/aula/com/aula8/extra/ArquivoUtil.java
+++ Aulas_ifc/src/br/edu/ifc/aula/com/aula8/extra/ArquivoUtil.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.edu.ifc.aula.com.aula8;
+package br.edu.ifc.aula.com.aula8.extra;
 
 import java.io.BufferedWriter;
 import java.io.File;
