9fb67f8c8ceb43283e4ec0a6638452ece187699e
aluno <aluno@lab2-pc06.local>
2019-04-03 19:42:07 -0300

    aula11

---
+++ Aulas_ifc/src/br/edu/ifc/aula/com/aula11/PrimeiraInterface.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifc.aula.com.aula11;
+
+import javax.swing.*;
+/**
+ *
+ * @author aluno
+ */
+public class PrimeiraInterface extends javax.swing.JFrame{
+    
+    
+    private JButton btnSalvar;
+
+    public PrimeiraInterface() {
+        super("Primeiro exemplo");
+        super.setSize(300,300);
+        super.setVisible(false);
+        super.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
