371b77f68971f73b0394b8f880166055218b374c
aluno <aluno@lab2-pc06.local>
2019-04-03 19:47:54 -0300

    aula11

--- Aulas_ifc/src/br/edu/ifc/aula/com/aula11/PrimeiraInterface.java
+++ Aulas_ifc/src/br/edu/ifc/aula/com/aula11/PrimeiraInterface.java
@@ -9,43 +9,33 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.*;
+
 /**
  *
  * @author aluno
  */
-public class PrimeiraInterface extends javax.swing.JFrame{
-    
-    
+public class PrimeiraInterface extends javax.swing.JFrame {
+
     private JButton btnSalvar;
 
     public PrimeiraInterface() {
         super("Primeiro exemplo");
-        super.setSize(300,300);
+        super.setSize(300, 300);
         super.setVisible(false);
         super.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        
+
     }
-    
-    
+
     public static void main(String[] args) {
-        try {
-            java.awt.EventQueue.invokeAndWait(new Runnable(){
-                @Override
-                public void run() {
-                    
-                    
-                }
-                
-                
-            });
-        } catch (InvocationTargetException ex) {
-            Logger.getLogger(PrimeiraInterface.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(PrimeiraInterface.class.getName()).log(Level.SEVERE, null, ex);
-        }
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+
+        });
+
     }
-    
-    
-    
-    
+
 }
