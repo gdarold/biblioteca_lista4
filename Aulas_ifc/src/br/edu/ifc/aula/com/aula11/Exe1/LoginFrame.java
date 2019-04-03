0e46b069236da20eec01fc6f69f84659aeb327c5
aluno <aluno@lab2-pc06.local>
2019-04-03 20:38:00 -0300

    aula11

---
+++ Aulas_ifc/src/br/edu/ifc/aula/com/aula11/Exe1/LoginFrame.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifc.aula.com.aula11.Exe1;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author aluno
+ */
+public class LoginFrame extends JFrame{
+    
+    
+    private JTextField txtLogin;
+    private JLabel lblSenha;
+    private JLabel lblLogar;
+    private JButton btnLogar;
+    private JButton btnCancelar;
+    private JPasswordField txtSenha;
+
+    public LoginFrame() {
+        
+        
+    }
+    
+    
+    private void initComponentes(){
+        
+        
+        
+    }
+    
+  private void initEventos(){
+      
+      
+      
+  }
+    
+    
+}
