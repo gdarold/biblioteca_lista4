b956a2d2ab54c9680b73da250bd55ad6d115b084
aluno <aluno@lab2-pc06.local>
2019-04-24 20:16:23 -0300

    aula17

---
+++ Aulas_ifc/src/br/edu/ifc/aula/com/aula17/ExemplosLista.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifc.aula.com.aula17;
+
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author aluno
+ */
+public class ExemplosLista {
+   
+    
+    public static void main(String[] args) {
+         java.util.List<String> aluno = new ArrayList();
+        
+         
+         aluno.add("aa");
+    }
+   
+    
+    
+  
+    
+    
+    
+}
